8a0867cb8c04610ba0b5ecc25763fd69d0cb3a20
ZhaoHoangLi <126206745+ZhaoHoangLi@users.noreply.github.com>
2024-08-20 11:56:54 +1000

    update code

--- My project ( Planet saver)/Assets/Scripts/PlayerAttack.cs
+++ My project ( Planet saver)/Assets/Scripts/PlayerAttack.cs
@@ -4,32 +4,15 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private float timeBtwAttack;
-    public float startTimeBtwAttack;
-
-    public Transform attackPos;
-    public LayerMask whatIsEnemies; 
-    public float attackRange; 
-    public int damage;
-
-    Void Update(){
-
-        if(timeBtwAttack <= 0){
-            // then you can attack
-            if(Input. Getkey(KeyCode.Space)){
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleA11( attackPos.position, attackRange, whatIsEnemies);
-                for (int i = 0; i < enemiesToDamage.Length; i++) {
-                    enemiesToDamage[i].GetComponent<Enemy>().health -= damage;
-                }
-            }
-
-            timeBtwAttack = startTimeBtwAttack;
-        } else {
-            timeBtwAttack -= Time.deltaTime;
-        }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
-    void OnDrawGizmosSelected(){
-
+    // Update is called once per frame
+    void Update()
+    {
         
-    }     
+    }
+}
