b6abccffb6b7768e7a860babde4bf12ad3f0f141
ZhaoHoangLi <126206745+ZhaoHoangLi@users.noreply.github.com>
2024-09-05 11:55:21 +1000

    update

---
+++ My project ( Planet saver)/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        // Play hurt animation
+
+        if(currentHealth <= 0)
+        {
+           Die(); 
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        // Die animation
+
+        // Disable the enemy
+    }
+
+}
--- My project ( Planet saver)/Assets/Scripts/PlayerCombat.cs
+++ My project ( Planet saver)/Assets/Scripts/PlayerCombat.cs
@@ -7,6 +7,13 @@ public class PlayerCombat : MonoBehaviour
 
     public Animator aimator;
 
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+    
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
+
     // Update is called once per frame
     void Update()
     {
@@ -22,7 +29,21 @@ public class PlayerCombat : MonoBehaviour
         aimator.SetTrigger("Attack");
 
         // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
         // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+           enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
 }
