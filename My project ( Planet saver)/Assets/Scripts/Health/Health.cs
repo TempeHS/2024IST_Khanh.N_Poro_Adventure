95e88d606e7e617b3f07fecccf79250e72319af4
Khanh N <khanh.nguyen120@education.nsw.gov.au>
2024-09-26 18:24:27 +1000

    added health collectible

--- My project ( Planet saver)/Assets/Scripts/Health/Health.cs
+++ My project ( Planet saver)/Assets/Scripts/Health/Health.cs
@@ -31,4 +31,8 @@ public class Health : MonoBehaviour
             }
         }
     }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
 }
